c3c8b30c2fba0eda1c4e8cd7be37e3fbb6d3eaa2
RafidFahar <62782723+RafidFahar@users.noreply.github.com>
2020-11-07 11:23:10 +0700

    Add Validation in customer.model

--- RentalKendaraan_045/Models/Customer.cs
+++ RentalKendaraan_045/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan_045.Models
 {
@@ -9,12 +10,21 @@ namespace RentalKendaraan_045.Models
         {
             Peminjaman = new HashSet<Peminjaman>();
         }
+        
 
         public int IdCustomer { get; set; }
+        [Required(ErrorMessage = "Nama Customer wajib diisi!")]
         public string NamaCustomer { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Nik hanya boleh diisi dengan angka")]
         public string Nik { get; set; }
+        [Required(ErrorMessage = "Alamat wajib diisi!")]
         public string Alamat { get; set; }
+
+        [MinLength(11, ErrorMessage = " No Hp tidak boleh kurang dari 10 angka")]
+        [MaxLength(13, ErrorMessage = " No Hp tidak boleh lebih dari 13 angka")]
+        [Required(ErrorMessage = "No Hp wajib diisi!")]
         public string NoHp { get; set; }
+        [Required(ErrorMessage = "Data Gender wajib diisi!")]
         public int? IdGender { get; set; }
 
         public Gender IdGenderNavigation { get; set; }
